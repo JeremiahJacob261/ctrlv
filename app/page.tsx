f61aab8e4893ff5347d3442544b26db429817301
Jerry Codes <69473368+JeremiahJacob261@users.noreply.github.com>
2025-01-16 09:52:48 +0000

    fgh

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ console.log(contentx,"hoookkm");
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center">Pastebin Replica</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center">CTRL V</h1>
         <div className="mb-4 text-center">
           <Link href="/list" className="text-blue-500 hover:underline">
             View All Pastes
